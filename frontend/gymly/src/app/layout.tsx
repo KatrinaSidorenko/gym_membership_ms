0177019e4db9fd4bf62afb93b74d3d1c87cabeb6
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-11-16 15:56:09 +0200

    Reapply "add basic project structures"
    This reverts commit 50b27f4baa21098614a67eb17f754567a5ce445b.

---
+++ frontend/gymly/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+import {NextUIProvider} from "@nextui-org/system";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <NextUIProvider>
+          {children}
+        </NextUIProvider>
+      </body>
+    </html>
+  );
+}
